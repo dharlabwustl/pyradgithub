7d4341ab405b375ef998f6a6ae6613a93680ceb6
dharlabwustl <atulkumar@wustl.edu>
2024-12-19 15:49:19 -0600

    Hello

--- call_pyradiomics.py
+++ call_pyradiomics.py
@@ -20,8 +20,8 @@ def call_pyradiomics(SESSION_ID,file_output_dir,mask_dir_and_ext):
     downloadfile_withasuffix(SESSION_ID,SCAN_ID,file_output_dir,'NIFTI','.nii')
     for each_ext in mask_dir_and_ext[1:]:
         downloadfile_withasuffix(SESSION_ID,SCAN_ID,file_output_dir,resource_dir,each_ext)
-        OUTPUT_DIRECTORY='workingoutput'
-        levelset2originalRF_new_flip_with_params(os.path.join('/input',SCAN_NAME.split('.nii')[0]+'.nii'), os.path.join('/input',SCAN_NAME.split('.nii')[0]+'_resaved_csf_unet.nii.gz'), '/workingoutput') #, mask_color=(0, 255, 0), image_prefix="original_ct_with_infarct_only", threshold=0.5)
+        OUTPUT_DIRECTORY='/workingoutput'
+        levelset2originalRF_new_flip_with_params(os.path.join('/input',SCAN_NAME.split('.nii')[0]+'.nii'), os.path.join('/input',SCAN_NAME.split('.nii')[0]+each_ext), '/workingoutput') #, mask_color=(0, 255, 0), image_prefix="original_ct_with_infarct_only", threshold=0.5)
 
     print (each_ext)
     return 1
