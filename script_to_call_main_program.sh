9bb8627e61cf5923f73c1713b1723793ab32a03c
dharlabwustl <atulkumar@wustl.edu>
2024-12-19 13:27:20 -0600

    Hello

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -2,12 +2,31 @@
 SESSION_ID=${1}
 XNAT_USER=${2}
 XNAT_PASS=${3}
-XNAT_HOST=${4}
-TYPE_OF_PROGRAM=${5}
-echo ${TYPE_OF_PROGRAM}::${SESSION_ID}::${XNAT_USER}::${XNAT_PASS}::${XNAT_HOST}
-if [[ ${TYPE_OF_PROGRAM} == "APPLY_PYRADIOMICS" ]] ;
-then
-  echo ${TYPE_OF_PROGRAM}
-/workspace/venv/bin/python /software/call_pyradiomics.py
-#    /software/processing_before_segmentation.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST  ##/input /output
+TYPE_OF_PROGRAM=${4}
+echo TYPE_OF_PROGRAM::${TYPE_OF_PROGRAM}
+#'https://redcap.wustl.edu/redcap/api/' #
+echo ${REDCAP_API}
+#export REDCAP_API=${6}
+#echo REDCAP_API::${REDCAP_API}
+# The input string
+input=$XNAT_HOST ##"one::two::three::four"
+# Check if '::' is present
+if echo "$input" | grep -q "+"; then
+  # Set the delimiter
+  IFS='+'
+
+  # Read the split words into an array
+  read -ra ADDR <<< "$input"
+  export XNAT_HOST=${ADDR[0]}
+  SUBTYPE_OF_PROGRAM=${ADDR[1]}
+else
+export XNAT_HOST=${5}
+    echo "'+' is not present in the string"
+fi
+
+
+echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM::${SUBTYPE_OF_PROGRAM}::${ADDR[0]}::${ADDR[2]}::${ADDR[3]}
+if [[ ${TYPE_OF_PROGRAM} == 'APPLY_PYRADIOMICS' ]]; then
+  echo "I AM HERE"
+#  /software/lin_transform_before_deepreg_mni_template.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
 fi
