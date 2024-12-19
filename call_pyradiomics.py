618464925cea093c3198ddce2168263c97eec7dc
dharlabwustl <atulkumar@wustl.edu>
2024-12-19 13:09:05 -0600

    Hello

--- call_pyradiomics.py
+++ call_pyradiomics.py
@@ -1,2 +1,21 @@
 from download_with_session_ID_Dec192024 import *
 print("I AM HERE")
+# Example usage if run directly
+if __name__ == "__main__":
+    import sys
+    print("I AM HERE")
+    #
+    # # Ensure input arguments are provided
+    # if len(sys.argv) != 3:
+    #     print("Usage: python run_pyradiomics.py <grayscale_image> <mask_image>")
+    #     sys.exit(1)
+    #
+    # gray_image = sys.argv[1]
+    # mask_image = sys.argv[2]
+    #
+    # try:
+    #     output_file = extract_radiomics_features(gray_image, mask_image)
+    #     print(f"Radiomics features saved to {output_file}")
+    # except Exception as e:
+    #     print(str(e))
+    #     sys.exit(1)
