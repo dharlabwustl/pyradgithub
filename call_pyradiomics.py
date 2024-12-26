79edb5a7bdc97d3fd00bba1e17b1b85a0b98a01b
dharlabwustl <atulkumar@wustl.edu>
2024-12-26 15:09:28 -0600

    Hello

--- call_pyradiomics.py
+++ call_pyradiomics.py
@@ -30,11 +30,16 @@ def call_pyradiomics(SESSION_ID,file_output_dir,mask_dir_and_ext):
         downloadfile_withasuffix(SESSION_ID,SCAN_ID,file_output_dir,resource_dir,each_ext)
         levelset2originalRF_new_flip_with_params(os.path.join('/input',SCAN_NAME.split('.nii')[0]+'.nii'), os.path.join('/input',SCAN_NAME.split('.nii')[0]+each_ext), '/workingoutput') #, mask_color=(0, 255, 0), image_prefix="original_ct_with_infarct_only", threshold=0.5)
     for each_ext in mask_dir_and_ext[1:]:
-        this_mask=glob.glob('/workingoutput/*'+each_ext)[0]
-        extract_radiomics_features(os.path.join('/input',SCAN_NAME.split('.nii')[0]+'.nii'), this_mask, output_csv=this_mask.split('.nii')[0]+'_radiomics.csv')
+        this_mask=glob.glob('/workingoutput/*'+each_ext) #[0]
+        for this_mask_each in this_mask:
+            extract_radiomics_features(os.path.join('/input',SCAN_NAME.split('.nii')[0]+'.nii'), this_mask_each, output_csv=this_mask_each.split('.nii')[0]+'_radiomics.csv')
         # downloadfile_withasuffix(SESSION_ID,SCAN_ID,file_output_dir,resource_dir,each_ext)
         # levelset2originalRF_new_flip_with_params(os.path.join('/input',SCAN_NAME.split('.nii')[0]+'.nii'), os.path.join('/input',SCAN_NAME.split('.nii')[0]+each_ext), '/workingoutput') #, mask_color=(0, 255, 0), image_prefix="original_ct_with_infarct_only", threshold=0.5)
-    for each_radiomic_file in glob.glob(os.path.dirname(this_mask)+'/*_radiomics.csv'):
+    # for each_radiomic_file in glob.glob(os.path.dirname(this_mask)+'/*_radiomics.csv'):
+    # for each_ext in mask_dir_and_ext[1:]:
+    #     this_mask=glob.glob('/workingoutput/*'+each_ext) #[0]
+    #     for this_mask_each in this_mask:
+    for each_radiomic_file in glob.glob(os.path.dirname(this_mask_each)+'/*_radiomics.csv'):
         try:
             print("Before UPLOAD SUCCESS")
             resource_dirname='RADIOMICS'
